0b55692e0061e32382fccc5df7c19951d6eccbf2
HectorQuinonesBarrios <a291528@uach.mx>
2016-04-03 20:23:17 -0600

    Expanded
    Agregadas paginas adicionales al proyecto

--- JavaScript/JavaScript.js
+++ JavaScript/JavaScript.js
@@ -1,3 +1,5 @@
-  function saludar(){
-    alert("e s t e t i c o");
-  }
+function saludar() {
+    "use strict";
+    alert("E s t e t i c o");
+    document.getElementById("bb").innerHTML = "E s t e t i c o";
+}
